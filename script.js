8b909b857219e9656ef77d069ba76e6f06d3713d
jesper53 <74552164+jesper-stack@users.noreply.github.com>
2021-09-22 07:55:07 +0000

    begin

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
+function setup() {
+  canvas = createCanvas(800, 500);
+  background(0, 187, 255);
+  frameRate(10);
+}
 
 
 
-
+function draw() {
+  noStroke();
+  fill(255, 230, 0);
+  ellipse(30, 220, 30);
+  stroke('green');
+  fill('white');
+  strokeWeight(10);
 
 }
